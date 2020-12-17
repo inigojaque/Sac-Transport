914e7eb506b32ce9e95fe42a0ca96f5deda59e66
Armen Fesliyan <armenfes@gmail.com>
2020-12-16 19:46:16 -0800

    fix map and survey

--- js/map.js
+++ js/map.js
@@ -1,3 +1,28 @@
+var runningLookup = false;
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async function queryGoogle() {
+
+
+
+  if (runningLookup) {
+    return;
+  }
+
+  runningLookup=true;
+    document.getElementById("returnStatus").innerHTML = "Loading";
+    document.getElementById("returnStatus").style.color = "orange";
+    //wait 3 seconds
+    await sleep(2500);
+    //press go btn
+    document.getElementById("Mode").click();
+
+    runningLookup=false;
+    document.getElementById("returnStatus").innerHTML = "";
+  }
+
     function initMap() {
         const directionsService = new google.maps.DirectionsService();
         const directionsRenderer = new google.maps.DirectionsRenderer();
@@ -14,7 +39,7 @@
         const onChangeHandler = function() {
             calculateAndDisplayRoute(directionsService, directionsRenderer);
         };
-        document.getElementById("nearest_lightrail").addEventListener("click", onChangeHandler);
+        document.getElementById("Mode").addEventListener("click", onChangeHandler);
     }
 
     function calculateAndDisplayRoute(directionsService, directionsRenderer) {
@@ -25,13 +50,22 @@
                 destination: {
                     query: "6000 J St, Sacramento, CA 95819",
                 },
-                travelMode: google.maps.TravelMode[document.getElementById('mode').value],
+                travelMode: google.maps.TravelMode[document.getElementById('Mode').value],
             },
             (response, status) => {
                 if (status === "OK") {
                     directionsRenderer.setDirections(response);
                 } else {
-                    window.alert("Directions request failed due to " + status);
+                  var found = status.includes("NOT_FOUND")
+                  if (found==true) {
+                      status="Address not found";
+                      document.getElementById("returnStatus").innerHTML = status;
+                      document.getElementById("returnStatus").style.color = "red";
+                  } else {
+                    document.getElementById("returnStatus").innerHTML = "";
+                  }
+
+
                 }
             }
         );
